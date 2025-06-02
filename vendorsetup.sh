6a03bb95a0772b796a52f40f471c159a27e79d71
GXC2356 <gxcdev2356@gmail.com>
2025-06-02 22:13:55 +0300

    Aosp adpations
    Signed-off-by: GXC2356 <gxcdev2356@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,21 @@
+# Common
+rm -rf device/samsung/gta4l-common
+git clone https://github.com/LineageOS/android_device_samsung_gta4l-common.git device/samsung/gta4l-common --depth=1
+
+# Vendor
+rm -rf vendor/samsung/gta4l
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_gta4l.git vendor/samsung/gta4l --depth=1
+
+# Vendor common
+rm -rf vendor/samsung/gta4l-common
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_gta4l-common.git vendor/samsung/gta4l-common --depth=1
+
+# Kernel
+rm -rf kernel/samsung/sm6115
+git clone https://github.com/LineageOS/android_kernel_samsung_sm6115.git kernel/samsung/sm6115 --depth=1
+
+# Hardware
+rm -rf hardware/samsung
+git clone https://github.com/LineageOS/android_hardware_samsung.git hardware/samsung
+rm -rf hardware/samsung/AdvancedDisplay
+rm -rf hardware/samsung/doze
